50874a375fa1327fc87d714745f2665c5efa6f8f
cmshouse <gg0514@gmail.com>
2024-04-06 00:56:00 +0900

    Code-behine의 리소스

--- Tutorial/Resource/MyWindow.xaml.cs
+++ Tutorial/Resource/MyWindow.xaml.cs
@@ -20,4 +20,11 @@ public partial class MyWindow : Window
     {
         InitializeComponent();
     }
+
+    private void btnClickMe_Click(object sender, RoutedEventArgs e)
+    {
+        lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+        lbResult.Items.Add(this.FindResource("strWindow").ToString());
+        lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+    }    
 }
