837890bdfdcbf214af612545bcc95d5dd5b5dfaa
cmshouse <gg0514@gmail.com>
2024-04-05 23:00:03 +0900

    StartupUri 대체

--- Tutorial/App.xaml.cs
+++ Tutorial/App.xaml.cs
@@ -1,13 +1,27 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Tutorial.Event;         // MyWindow의 namespace
 
-namespace Tutorial;
+
+
+namespace Tutorial;           // App Class의 namespace
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        // Create the startup window
+        MyWindow wnd = new MyWindow();
+        // Do stuff here, e.g. to the window
+        wnd.Title = "StartupUri 대체";
+        // Show the window
+        wnd.Show();
+    }
+
 }
 
